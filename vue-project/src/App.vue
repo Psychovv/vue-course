b48503ea97101e91900ca3920426a954b46b28ce
Psychovv <viniciusmedina63@gmail.com>
2026-05-12 08:50:09 -0300

    add: adicionando landing page, ajustando importacoes de imagens e ajustando estilizacao

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,3 +1,15 @@
     <template>
-      <h1>Meu primeiro projeto Vue!</h1>
+      <header class="banner">
+        <div class="apresentacao">
+          <img src="./assets/imagens/logo.svg" alt="Logo do cooking up" class="logo">
+
+          <p class ="cabecalho frase-cabecalho">
+            <span class="texto-verde">Um banquete de ideias</span>
+            despertar o chef que ha em voce!
+          </p>
+
+          <p class="subtitulo-lg">Explore novas receitas todos os dias com os ingredientes que estao ao seu alcance!</p>
+        </div>
+        <img src="./assets/imagens/foto-banner.png" alt="Foto do banner da mulher cozinhando coim a tigela na mao" class="foto-banner">
+      </header>
     </template>
