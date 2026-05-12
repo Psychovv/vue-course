712ab267e44b495b9b43aac506792710331335b3
Psychovv <viniciusmedina63@gmail.com>
2026-05-12 10:57:01 -0300

    feat: criado componente conteudo principal.vue e importado na app.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,14 +1,18 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './ConteudoPrincipal.vue';
 
 export default {
-  components: { Banner }
+  components: { Banner, ConteudoPrincipal }
 }
 </script>
 
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
+
+
 
   <style scoped>
   </style>
