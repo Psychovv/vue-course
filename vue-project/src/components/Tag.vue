4c982a6a7c0f452f1b0af0635c30856eee5475aa
Psychovv <viniciusmedina63@gmail.com>
2026-05-12 15:08:34 -0300

    feat: adicionando a prop ativa ao component Tag.vue

--- vue-project/src/components/Tag.vue
+++ vue-project/src/components/Tag.vue
@@ -2,13 +2,14 @@
 export default {
   name: 'Tag',
   props: {
-    texto: { type: String, required: true }
+    texto: { type: String, required: true },
+    ativa: { type: Boolean }
   }
 }
 </script>
 
 <template>
-    <span class="tag">
+    <span class="tag" :class="{ ativa }">
         {{ texto }}
     </span>
 </template>
@@ -21,6 +22,12 @@ export default {
   padding: 0.5rem;
   text-align: center;
   transition: 0.2s;
+  color: var(--cinza, #444);
+  background:var(--cinza-claro, #4e4d4d); ;
+  font-weight: 00;
+}
+
+.tag.ativa{
   color: var(--creme, #FFFAF3);
   background: var(--coral, #F0633C);
   font-weight: 700;
